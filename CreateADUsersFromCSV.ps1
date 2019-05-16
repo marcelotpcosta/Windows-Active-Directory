87eae23b754f98a3b83fbf10b1625160723680e1
Marcelo Costa <marcelotpcosta@outlook.com>
2019-05-15 23:42:25 -0300

    Update CreateADUsersFromCSV.ps1

--- CreateADUsersFromCSV.ps1
+++ CreateADUsersFromCSV.ps1
@@ -5,13 +5,13 @@ Enter-PSSession -ComputerName <dc.local> -Credential <DOMAIN\user.name>
 Import-Module ActiveDirectory
 
 $Users = Import-Csv -Delimiter "," -Path "C:\Users.csv"
-foreach ($Linha in $Users)
+foreach ($Line in $Users)
 {  
-    $Password = $Linha.Password
-    $Detailedname = $Linha.Firstname + " - " + $Linha.SecName
-    $UserFirstname = $Linha.Firstname
-    $SAM =  $Linha.Login
+    $Password = $Line.Password
+    $Detailedname = $Line.Firstname + " - " + $Linha.SecName
+    $UserFirstname = $Line.Firstname
+    $SAM =  $Line.Login
     $OU = "OU=Users,OU=DPTO,OU=ENV,DC=EMP,DC=LOCAL" 
-    New-ADUser -Name $Detailedname -SamAccountName $SAM -UserPrincipalName $SAM -DisplayName $Detailedname -GivenName $Linha.Firstname -Surname $Linha.SecName -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path $OU -Description $Linha.Description
+    New-ADUser -Name $Detailedname -SamAccountName $SAM -UserPrincipalName $SAM -DisplayName $Detailedname -GivenName $Line.Firstname -Surname $Line.SecName -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path $OU -Description $Line.Description
     
 }
